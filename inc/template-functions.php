fb9b9688896dfa590bb8d8174ac161ffebd5b9ef
Thomas Weichselbaumer <kontakt@themecoder.de>
2019-02-06 14:40:39 +0100

    added remove bottom margin setting to page options

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -26,6 +26,11 @@ function gt_basic_body_classes( $classes ) {
 		$classes[] = 'page-title-hidden';
 	}
 
+	// Remove bottom margin of page?
+	if ( is_page() && get_post_meta( get_the_ID(), 'gt_remove_bottom_margin', true ) ) {
+		$classes[] = 'page-bottom-margin-removed';
+	}
+
 	// Adds a class of hfeed to non-singular pages.
 	if ( ! is_singular() ) {
 		$classes[] = 'hfeed';
