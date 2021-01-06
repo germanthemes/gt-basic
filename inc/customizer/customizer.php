6a615d76e58759db90cb99093f484693054a84f6
Thomas Weichselbaumer <kontakt@themecoder.de>
2021-01-06 21:33:06 +0100

    updated typography options to use WPTT Webfont loader

--- inc/customizer/customizer.php
+++ inc/customizer/customizer.php
@@ -49,15 +49,7 @@ add_action( 'customize_register', 'gt_basic_customize_register_options' );
  * Embed JS file to make Theme Customizer preview reload changes asynchronously.
  */
 function gt_basic_customize_preview_js() {
-	wp_enqueue_script( 'gt-basic-customize-preview', get_template_directory_uri() . '/assets/js/customize-preview.min.js', array( 'customize-preview' ), '20191212', true );
-
-	// Load Custom Fonts from GT Local Fonts plugin in Customizer Preview if plugin is active.
-	if ( class_exists( 'GermanThemes_Local_Fonts' ) ) {
-		$font_settings = array(
-			'pluginURL' => WP_PLUGIN_URL . '/gt-local-fonts/assets/css/',
-		);
-		wp_localize_script( 'gt-basic-customize-preview', 'gtBasicFontSettings', $font_settings );
-	}
+	wp_enqueue_script( 'gt-basic-customize-preview', get_template_directory_uri() . '/assets/js/customize-preview.js', array( 'customize-preview' ), '20210106', true );
 }
 add_action( 'customize_preview_init', 'gt_basic_customize_preview_js' );
 
